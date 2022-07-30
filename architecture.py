9baefa421039f3aa103059b5984e9b16515da2c9
Gphysics-fpf <guillermof.pens.fdez@gmail.com>
2022-07-30 14:54:42 +0200

    Concatenate bases and trajectories

--- architecture.py
+++ architecture.py
@@ -1,4 +1,5 @@
-from typing import Union, Callable
+from typing import Optional, Union, Callable
+from dataclasses import dataclass
 from math import pi as π
 from numbers import Number
 import numpy as np
@@ -9,68 +10,54 @@ from bosons import (
     move_excitation_operator,
     diagonals_with_energies,
     concatenate_bases,
-    erase,
 )
 
 
+@dataclass
 class Exp_2qubits_2cavities_2purcells:
-    def __init__(
-        self,
-        δ1: float = 2 * π * 8.406,
-        δ2: float = 2 * π * 8.406,
-        g1: Union[Number, Callable] = 2 * π * 0.0086,
-        g2: Union[Number, Callable] = 2 * π * 0.0086,
-        gp1: float = 2 * π * 0.025,
-        gp2: float = 2 * π * 0.025,
-        ω1: float = 2 * π * 8.406,
-        ω2: float = 2 * π * 8.406,
-        ωp1: float = 2 * π * 8.406,
-        ωp2: float = 2 * π * 8.406,
-        κ1: float = 2 * π * 0.0086,
-        κ2: float = 2 * π * 0.0086,
-        δLamb: float = 0.0,
-        Nexcitations: int = 1,
-        include_vacuum: bool = False,
-        waveguide: Waveguide = None,
-    ):
+    """Architecture with 2 qubits, 2 cavities, 2 Purcell filters and waveguide.
+    The disposition of this setup is 1qb + 1cav + 1 Pfil + WG and the same at the other end
+    ..."""
 
-        """Architecture with 2 qubits, 2 cavities, 2 Purcell filters and waveguide.
-        The disposition of this setup is 1qb + 1cav + 1 Pfil + WG and the same at the other end
-        ..."""
-        self.δ1 = δ1
-        self.δ2 = δ2
-        self.δLamb = δLamb
-        self.g1 = g1
-        self.g2 = g2
-        self.gp1 = gp1
-        self.gp2 = gp2
-        self.ω1 = ω1
-        self.ω2 = ω2
-        self.ωp1 = ωp1
-        self.ωp2 = ωp2
-        self.κ1 = κ1
-        self.κ2 = κ2
-        self.Nexcitations = Nexcitations
-        self.Nqubits = 2
-        self.Ncavs = 2
-        self.Nfilters = 2
+    δ1: float = 2 * π * 8.406
+    δ2: float = 2 * π * 8.406
+    g1: Union[Number, Callable] = 2 * π * 0.0086
+    g2: Union[Number, Callable] = 2 * π * 0.0086
+    gp1: float = 2 * π * 0.025
+    gp2: float = 2 * π * 0.025
+    ω1: float = 2 * π * 8.406
+    ω2: float = 2 * π * 8.406
+    ωp1: float = 2 * π * 8.406
+    ωp2: float = 2 * π * 8.406
+    κ1: float = 2 * π * 0.0086
+    κ2: float = 2 * π * 0.0086
+    δLamb: float = 0.0
+    Nexcitations: int = 1
+    Up_to_Nexcitations: Optional[int] = None
+    waveguide: Optional[Waveguide] = None
+    Nqubits: int = 2
+    Nfilters: int = 2
+    Ncavs: int = 2
+
+    def __post_init__(self):
+
         self.qubit_indices = np.arange(0, self.Nqubits)
         self.cav_indices = np.arange(self.Nqubits, self.Nqubits + self.Ncavs)
         self.filter_indices = np.arange(
             self.Nqubits + self.Ncavs, self.Nqubits + self.Ncavs + self.Nfilters
         )
 
-        if waveguide is None:
-            waveguide = Waveguide(frequency=self.ω1 + self.δLamb, length=5, modes=351)
-        self.waveguide = WG = waveguide
+        if self.waveguide is None:
+            self.waveguide = Waveguide(
+                frequency=self.ω1 + self.δLamb, length=5, modes=351
+            )
+        waveguide = WG = self.waveguide
 
-        if include_vacuum:
+        if self.Up_to_Nexcitations is not None:
             self.Basis = concatenate_bases(
-                Nqubits=self.Nqubits,
-                Ncavs=self.Ncavs,
-                Nfilters=self.Nfilters,
-                N_WGmodes=waveguide.modes,
-                Up_to_Nexcitations=self.Nexcitations,
+                qubits=self.Nqubits,
+                bosons=self.Ncavs + self.Nfilters + waveguide.modes,
+                Up_to_Nexcitations=self.Up_to_Nexcitations,
             )
             self.first_WGmode = len(self.Basis) - 1 - waveguide.modes
 
@@ -123,7 +110,7 @@ class Exp_2qubits_2cavities_2purcells:
             )
         )
 
-        gp = np.array([gp1, gp2])
+        gp = np.array([self.gp1, self.gp2])
         H_cavity_purcell = sum(
             [
                 g
@@ -181,38 +168,28 @@ class Exp_2qubits_2cavities_2purcells:
 
 
 class Exp_2qubits_2cavities:
-    def __init__(
-        self,
-        δ1=2 * π * 8.406,
-        δ2=2 * π * 8.406,
-        g1=2 * π * 0.0086,  # Union[number, callable]
-        g2=2 * π * 0.0086,
-        ω1=2 * π * 8.406,
-        ω2=2 * π * 8.406,
-        κ1=2 * π * 0.0086,
-        κ2=2 * π * 0.0086,
-        δLamb=0.0,
-        Nexcitations=1,
-        include_vacuum=False,
-        waveguide=None,
-    ):
+
+    δ1: float = 2 * π * 8.406
+    δ2: float = 2 * π * 8.406
+    g1: Union[Number, Callable] = 2 * π * 0.0086
+    g2: Union[Number, Callable] = 2 * π * 0.0086
+    ω1: float = 2 * π * 8.406
+    ω2: float = 2 * π * 8.406
+    κ1: float = 2 * π * 0.0086
+    κ2: float = 2 * π * 0.0086
+    δLamb: float = 0.0
+    Nexcitations: int = 1
+    Up_to_Nexcitations: Optional[int] = None
+    waveguide: Optional[Waveguide] = None
+    Nqubits: int = 2
+    Ncavs: int = 2
+    Nfilters: int = 0
+
+    def __post_init__(self):
 
         """Architecture with 2 qubits, 2 cavities, 2 Purcell filters and waveguide.
         The disposition of this setup is 1qb + 1cav + 1 Pfil + WG and the same at the other end
         ..."""
-        self.δ1 = δ1
-        self.δ2 = δ2
-        self.δLamb = δLamb
-        self.g1 = g1
-        self.g2 = g2
-        self.ω1 = ω1
-        self.ω2 = ω2
-        self.κ1 = κ1
-        self.κ2 = κ2
-        self.Nexcitations = Nexcitations
-        self.Nqubits = 2
-        self.Ncavs = 2
-        self.Nfilters = 2
         self.qubit_indices = np.arange(0, self.Nqubits)
         self.cav_indices = np.arange(self.Nqubits, self.Nqubits + self.Ncavs)
 
@@ -220,24 +197,21 @@ class Exp_2qubits_2cavities:
             waveguide = Waveguide(frequency=self.ω1 + self.δLamb, length=5, modes=351)
         self.waveguide = WG = waveguide
 
-        if include_vacuum:
+        if self.Up_to_Nexcitations is not None:
             self.Basis = concatenate_bases(
-                Nqubits=self.Nqubits,
-                Ncavs=self.Ncavs,
-                N_WGmodes=waveguide.modes,
-                Up_to_Nexcitations=self.Nexcitations,
+                qubits=self.Nqubits,
+                bosons=self.Ncavs + self.Nfilters + waveguide.modes,
+                Up_to_Nexcitations=self.Up_to_Nexcitations,
             )
             self.first_WGmode = len(self.Basis) - 1 - waveguide.modes
 
         else:
             self.Basis = construct_basis(
                 qubits=self.Nqubits,
-                bosons=self.Ncavs + waveguide.modes,
+                bosons=self.Ncavs + self.Nfilters + waveguide.modes,
                 excitations=self.Nexcitations,
             )
             self.first_WGmode = len(self.Basis) - waveguide.modes
-
-        self.size = len(self.Basis)
 
         """ Construct the vector of energies E for filling in the diagonal entries. """
 
--- bosons.py
+++ bosons.py
@@ -3,6 +3,7 @@ from typing import Iterable
 import numpy as np
 import scipy.sparse as sp
 
+
 #%matplotlib widget
 
 """ Routines for the construction of the Operators in Hilbert space """
@@ -39,6 +40,38 @@ def construct_basis(
             )
         )
     }
+
+
+def unphysical_states(Basis_element: tuple, Nqubits: int) -> bool:
+
+    """Checks wether in a given qubit position there is more than one
+    excitation and returns True in such case.
+
+    Args:
+        Basis_element (tuple): Number of qubits >= 0
+        Nqubits (int): Number of bosonic modes >= 0
+
+    Returns:
+        bool: False if state is physical, True if it is not.
+    """
+
+    if (
+        Basis_element[0] >= Nqubits
+    ):  # If the fist component is not qubit, then there no qubit excitations in this state. Therefore physical.
+
+        return False
+
+    for i in range(
+        len(Basis_element) - 1
+    ):  # If there are qubits run through the components.
+
+        if (
+            Basis_element[i] == Basis_element[i + 1]
+            and Basis_element[i] < Nqubits
+            and Basis_element[i + 1] < Nqubits
+        ):  # If two qubit excitations are consecutive, unphysical. return true.
+
+            return True
 
 
 def move_excitation_operator(
@@ -106,41 +139,9 @@ def diagonals_with_energies(basis: dict, frequencies: np.ndarray) -> sp.dia_matr
     return sp.diags(energy)
 
 
-def unphysical_states(Basis_element: tuple, qubits: int) -> bool:
-    """Function
-
-    Args:
-        Basis_element (tuple): _description_
-        qubits (int): _description_
-
-    Returns:
-        bool: _description_
-    """
-
-    if (
-        Basis_element[0] >= qubits
-    ):  # If the fist component is not qubit, then there no qubit excitations in this state. Therefore physical.
-
-        return False
-
-    for i in range(
-        len(Basis_element) - 1
-    ):  # If there are qubits run through the components.
-
-        if (
-            Basis_element[i] == Basis_element[i + 1]
-            and Basis_element[i] < qubits
-            and Basis_element[i + 1] < qubits
-        ):  # If two qubit excitations are consecutive, unphysical. return true.
-
-            return True
-
-
 def concatenate_bases(
     qubits: int = 2,
-    Ncavs: int = 0,
-    Nfilters: int = 0,
-    N_WGmodes: int = 30,
+    bosons: int = 30,
     Up_to_Nexcitations: int = 2,
 ) -> dict:
 
@@ -148,32 +149,24 @@ def concatenate_bases(
     constructs the basis resulting from the concatenation of subspaces up to these Nexcitations'.
 
     Args:
-        origin (int): Index of the origin mode
-        destination (int): Index of the destination mode
-        basis (dict): Collection of physical states (see: construct_basis)
+        qubits (int): Number of qubits >= 0
+        bosons (int): Number of bosonic modes >= 0
+        Up_to_Nexcitations (int): Number of excitations of the biggest subspace >= 0
 
     Returns:
-        Operator (sp.csr_matrix): Matrix representation of the quantum operator
+        basis: Collection of all the states that constitute the basis properly sorted.
     """
+    Basis = {}
 
-    N_modes = qubits + Ncavs + Nfilters + N_WGmodes
+    for excitations in range(Up_to_Nexcitations + 1):
 
-    Basis = {}  # We initialize the variable Basis, a dictionary.
-
-    for Nexcitations in range(
-        Up_to_Nexcitations + 1
-    ):  # Loop that runs over all the subspaces
-
-        if Nexcitations == 0:
+        if excitations == 0:
 
             Basis[
                 ()
             ] = 0  # For the subspace of 0 excitations we manually create the empty tuple corresponding to vacuum.
 
-        else:  # For all the non-trivial subspaces we compute a basis by calling the itertools tool
-
-            """filterfalse discards the true outputs of unphysical_states when run over the iterable produced by combinations with replacements."""
-
+        else:
             Basis_subspace = (
                 {}
             )  # We initialize the variable Basis for a particular subspace
@@ -182,26 +175,42 @@ def concatenate_bases(
                 Basis
             )  # Very important. The indexation of the new subspace must begin where the previous left
 
+            def make_bosonic_states(n_modes: int, excitations: int):
+                return itertools.combinations_with_replacement(
+                    np.arange(n_modes), excitations
+                )
+
+            def select_hardcore_boson_states(qubits: int, states: Iterable) -> Iterable:
+                return itertools.filterfalse(
+                    lambda x: unphysical_states(x, qubits), states
+                )
+
             for i, v in enumerate(
-                list(
-                    itertools.filterfalse(
-                        lambda x: unphysical_states(x, qubits),
-                        itertools.combinations_with_replacement(
-                            np.arange(N_modes), Nexcitations
-                        ),
-                    )
+                select_hardcore_boson_states(
+                    qubits, make_bosonic_states(qubits + bosons, excitations)
                 )
             ):
-
                 Basis_subspace[v] = i + index_0
 
-            Basis.update(Basis_subspace)  # Update is a sort of append for dictionaries.
+                Basis.update(
+                    Basis_subspace
+                )  # Update is a sort of append for dictionaries.
 
     return Basis
 
 
 def erase(remove: int, Basis: dict) -> sp.csr_matrix:
 
+    """Creates a sparse matrix representation of an operator that erases an excitation
+    from 'remove'.
+
+    Args:
+        remove (int): Index of the mode from which the excitation is going to be removed
+        destination (int): Index of the destination mode
+        basis (dict): Collection of physical states (see: construct_basis)
+
+    Returns:
+        Operator (sp.csr_matrix): Matrix representation of the quantum operator"""
     row = []
     column = []
     coefficient = []
